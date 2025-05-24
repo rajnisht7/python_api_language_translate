9d3b5f983077ee6c596a3b4642882f1d3773cdce
rajnisht7 <rajnishtiwari9787@gmail.com>
2025-05-24 12:48:49 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from googletrans import Translator
+
+app = Flask(__name__)
+
+@app.route("/translatelanguage")
+def translate_language():
+    text = request.args.get("text")
+    target = request.args.get("target")
+
+    if not text or not target:
+        return "Missing 'text' or 'target' parameter", 400
+
+    translator = Translator()
+    translated = translator.translate(text, dest=target)
+    result = {"text": translated.text}
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
